77a5737301de313fa37b1af9452a70657e33f3ea
FelixS25R <feli14871@gmail.com>
2026-06-06 20:18:08 +0200

    change file name

---
+++ Baregn1UI.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+colllum1= 10
+
+
+
+
+
+app = tk.Tk()
+
+app.geometry("800x300") 
+
+vektor1 = tk.Label(app, text="indtast vektor 1", font=("Arial", 16))
+vektor1.pack(pady=20)
+vektor1.place(x=colllum1, y=20)
+
+app.mainloop()
