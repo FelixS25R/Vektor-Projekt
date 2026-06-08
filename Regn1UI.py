d0a723482b1eb4bb4f57113f5a277ac6386f9ce9
FelixS25R <feli14871@gmail.com>
2026-06-08 08:49:11 +0200

    add entry

--- Regn1UI.py
+++ Regn1UI.py
@@ -10,8 +10,11 @@ app = tk.Tk()
 
 app.geometry("800x300") 
 
-vektor1 = tk.Label(app, text="indtast vektor 1", font=("Arial", 16))
+vektor1 = tk.Label(app, text="indtast vektor 1:", font=("Arial", 16))
 vektor1.pack(pady=20)
 vektor1.place(x=colllum1, y=20)
+
+x1 = tk.Entry(app)
+x1.place(x=colllum1, y=60)
 
 app.mainloop()
