cdce76a53abcb4bade819487afba2d4237755452
FelixS25R <feli14871@gmail.com>
2026-06-08 09:17:58 +0200

    add entry

--- Regn1UI.py
+++ Regn1UI.py
@@ -1,20 +1,60 @@
 import tkinter as tk
 
-colllum1= 10
-
-
+collum1= 40
+collum2=400
 
 
 
 app = tk.Tk()
 
-app.geometry("800x300") 
+app.geometry("1000x500") 
 
 vektor1 = tk.Label(app, text="indtast vektor 1:", font=("Arial", 16))
 vektor1.pack(pady=20)
-vektor1.place(x=colllum1, y=20)
+vektor1.place(x=collum1, y=20)
 
-x1 = tk.Entry(app)
-x1.place(x=colllum1, y=60)
+
+vektor2 = tk.Label(app, text="indtast vektor 2:", font=("Arial", 16))
+vektor2.pack(pady=20)
+vektor2.place(x=collum2, y=20)
+
+
+
+x1 = tk.Entry(app, font=("Arial", 16))
+x1.pack(pady=10)
+x1.place(x=collum1, y=60)
+
+y1 = tk.Entry(app, font=("Arial", 16))
+y1.pack(pady=10)
+y1.place(x=collum1, y=100)
+
+
+
+x2 = tk.Entry(app, font=("Arial", 16))
+x2.pack(pady=10)
+x2.place(x=collum2, y=60)
+
+y2 = tk.Entry(app, font=("Arial", 16))
+y2.pack(pady=10)
+y2.place(x=collum2, y=100)
+
+
+
+
+
+
+v1=[x1, y1]
+v2=[x2, y2]
+
+beregn = tk.Button(app, text="beregn", font=("Arial", 16))
+beregn.place(x=350, y=200)
+
+
+
+
+
+
+
+
 
 app.mainloop()
