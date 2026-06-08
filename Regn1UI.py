4e1844409070f667da0d7522db7a209a946729ad
FelixS25R <feli14871@gmail.com>
2026-06-08 10:19:51 +0200

    restrutere

--- Regn1UI.py
+++ Regn1UI.py
@@ -11,13 +11,7 @@ app.geometry("1000x500")
 
 
 
-variable = tk.StringVar(app)
-variable.set("plus") # default value
 
-
-w = tk.OptionMenu(app, variable, "plus", "minus", "gange")
-w.pack()
-w.place(x=350, y=170)
 
 
 
@@ -25,11 +19,6 @@ w.place(x=350, y=170)
 vektor1 = tk.Label(app, text="indtast vektor 1:", font=("Arial", 16))
 vektor1.pack(pady=20)
 vektor1.place(x=collum1, y=20)
-
-
-vektor2 = tk.Label(app, text="indtast vektor 2:", font=("Arial", 16))
-vektor2.pack(pady=20)
-vektor2.place(x=collum2, y=20)
 
 
 
@@ -40,6 +29,14 @@ x1.place(x=collum1, y=60)
 y1 = tk.Entry(app, font=("Arial", 16))
 y1.pack(pady=10)
 y1.place(x=collum1, y=100)
+
+
+
+
+vektor2 = tk.Label(app, text="indtast vektor 2:", font=("Arial", 16))
+vektor2.pack(pady=20)
+vektor2.place(x=collum2, y=20)
+
 
 
 
@@ -56,14 +53,23 @@ y2.place(x=collum2, y=100)
 
 
 
+
 v1=[x1, y1]
 v2=[x2, y2]
+
+
 
 beregn = tk.Button(app, text="beregn", font=("Arial", 16))
 beregn.place(x=350, y=200)
 
 
+variable = tk.StringVar(app)
+variable.set("plus") # default value
 
+
+w = tk.OptionMenu(app, variable, "plus", "minus", "gange")
+w.pack()
+w.place(x=350, y=170)
 
 
 
