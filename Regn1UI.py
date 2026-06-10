1d7d7711b7430ca7c947dc7b12207059ffd56ce8
FelixS25R <feli14871@gmail.com>
2026-06-10 14:52:12 +0200

    Update Regn1UI.py

--- Regn1UI.py
+++ Regn1UI.py
@@ -4,39 +4,45 @@ from Vektor_math import *
 
 collum1= 40
 collum2=400
-
+collum3=700
 
 
 app = tk.Tk()
 app.geometry("1000x500") 
 
+app.configure(bg="grey")
 
-
-vektor1 = tk.Label(app, text="indtast vektor 1:", font=("Arial", 16))
+vektor1 = tk.Label(app, text="indtast vektor 1:", font=("inconsolata", 16))
 vektor1.pack(pady=20)
 vektor1.place(x=collum1, y=20)
 
-x1 = tk.Entry(app, font=("Arial", 16))
+x1 = tk.Entry(app, font=("inconsolata", 16))
 x1.pack(pady=10)
 x1.place(x=collum1, y=60)
 
-y1 = tk.Entry(app, font=("Arial", 16))
+y1 = tk.Entry(app, font=("inconsolata", 16))
 y1.pack(pady=10)
 y1.place(x=collum1, y=100)
 
-vektor2 = tk.Label(app, text="indtast vektor 2:", font=("Arial", 16))
+vektor2 = tk.Label(app, text="indtast vektor 2:", font=("inconsolata", 16))
 vektor2.pack(pady=20)
 vektor2.place(x=collum2, y=20)
 
-x2 = tk.Entry(app, font=("Arial", 16))
+x2 = tk.Entry(app, font=("inconsolata", 16))
 x2.pack(pady=10)
 x2.place(x=collum2, y=60)
 
-y2 = tk.Entry(app, font=("Arial", 16))
+y2 = tk.Entry(app, font=("inconsolata", 16))
 y2.pack(pady=10)
 y2.place(x=collum2, y=100)
 
+skalar = tk.Label(app, text="indtast skalar:", font=("inconsolata   ", 16))
+skalar.pack(pady=20)
+skalar.place(x=collum3, y=20)
 
+S = tk.Entry(app, font=("inconsolata", 16))
+S.pack(pady=10)
+S.place(x=collum3, y=60)
 
 
 variable = tk.StringVar(app)
@@ -62,6 +68,9 @@ funktioner = {
 }
 
 
+
+v1=[x1, y1]
+v2=[x2, y2]
 
 def beregn(v1, v2):
     v1 = [float(x1.get()), float(y1.get())]
